9b744ca53b3a99fa6e1007e2acaf7b4734cc2536
Raghu <raghu@Raghus-MacBook-Pro.local>
2020-06-05 18:36:28 +0530

    Updates

---
+++ precache-manifest.a8627d0ee8f91f1a92581ed0e6e64fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3c125c5661ab0bb821664757bf636d",
+    "url": "/chain-cert/index.html"
+  },
+  {
+    "revision": "b4fc3676dcea223a7061",
+    "url": "/chain-cert/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "478001702a19e916edb8",
+    "url": "/chain-cert/static/css/main.ce512771.chunk.css"
+  },
+  {
+    "revision": "b4fc3676dcea223a7061",
+    "url": "/chain-cert/static/js/2.65a7f6df.chunk.js"
+  },
+  {
+    "revision": "478001702a19e916edb8",
+    "url": "/chain-cert/static/js/main.da13c3ee.chunk.js"
+  },
+  {
+    "revision": "0fd7fcfae571af474422",
+    "url": "/chain-cert/static/js/runtime-main.e9fda021.js"
+  }
+]);
